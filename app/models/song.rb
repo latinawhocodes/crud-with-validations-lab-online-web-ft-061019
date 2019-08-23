061c458706bdd835e40d87888e64358b0f3747b6
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-08-22 22:08:42 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
-    validates :title, presence: true
-    validates :title, uniqueness: { scope: %i[artist_name release_year] }
+    validates :title, presence: true, uniqueness: { scope: %i[artist_name release_year] }
     validates :release_year, presence: true, if: :released
     validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
 end
